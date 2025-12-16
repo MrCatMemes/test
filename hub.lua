2c965dce62fc1dca1869f1d827e7c96cbce6ae62
MrCatMemes <l.berndt2020@gmail.com>
2025-12-16 14:08:14 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,18 +1,1 @@
--- Hub Testscript
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/MrCatMemes/test/main/orionlib.lua"))()
 
--- Erstelle Fenster
-local Window = OrionLib:MakeWindow({
-    Name = "🚀 Test Hub | by MrCatMemes 😎"
-})
-
--- Erstelle Tab
-local Tab = Window:MakeTab({
-    Name = "Main"
-})
-
--- Test Label
-Tab:AddLabel("✅ OrionLib geladen!")
-
--- Init
-OrionLib:Init()
